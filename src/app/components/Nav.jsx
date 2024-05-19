b53f2e992cec2fc432dae9558571583c0004ae90
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-05-19 15:52:32 +0900

    Redesign Hero

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
-import { Hamburger } from '@phosphor-icons/react/dist/ssr';
+import { List } from '@phosphor-icons/react/dist/ssr';
 import Image from "next/image";
 
 export default function Nav() {
@@ -19,31 +19,38 @@ export default function Nav() {
     const mappedPages = pages.map(page => {
         return (
             <li key={page}>
-                <Link className={`${pathname.includes(page) || (pathname === '/' && page === 'home') ? 'font-extrabold' : ''} hover:underline underline-offset-4`} href={page === 'home' ? '/' : page}>{page.charAt(0).toUpperCase() + page.slice(1)}</Link>
+                <Link
+                    className={`${pathname.includes(page) || (pathname === '/' && page === 'home') ? 'font-extrabold' : ''} hover:underline underline-offset-4`}
+                    href={page === 'home' ? '/' : page}>{page.charAt(0).toUpperCase() + page.slice(1)}
+                </Link>
             </li>
         )
     })
 
+    const logo = <Image src='./logo.svg' width={30} height={30} alt="logo" />;
+
     return (
         <div className='relative'>
-
-            <div onClick={handleToggle} className="hover:cursor-pointer select-none absolute sm:hidden z-10 m-2">
-                {<Hamburger
+            <div onClick={handleToggle} className="hover:cursor-pointer select-none absolute sm:hidden z-10 mx-8 my-4">
+                {<List
                     size={40}
                     weight="light"
                 />}
             </div>
-            <nav className={`bg-blue-950 ${navShow ? 'block absolute sm:static w-screen opacity-95' : 'hidden'} p-7 sm:block`}>
+            <div className="absolute top-4 right-8 sm:hidden">
+                {logo}
+            </div>
+            <nav className={`bg-neutral-50/90 dark:bg-neutral-800/95 ${navShow ? 'block absolute sm:static w-screen' : 'hidden'} p-7 sm:block`}>
                 <ul className={`flex flex-col items-center text-center gap-2 sm:flex-row sm:gap-4`}>
-                    <div className="absolute right-4 top-2 sm:static">
-                        <Image src='./logo.svg' width={30} height={30} alt="logo" />
+                    <div className="hidden sm:block mr-4">
+                        {logo}
                     </div>
                     {mappedPages}
-                    <li className="mt-4 sm:mt-0 sm:ml-auto px-4 py-1 rounded-md bg-blue-100 text-black transition ease-in-out hover:-translate-y-1 w-24 font-medium">
-                        <Link href={'login'} className="">Log In</Link>
+                    <li className="text-neutral-700 bg-neutral-300 mt-4 sm:mt-0 sm:ml-auto px-4 py-2 rounded-md transition ease-in-out hover:-translate-y-1 font-medium">
+                        <Link href={'login'} className="">เข้าสู่ระบบ</Link>
                     </li>
-                    <li className="sm:mt-0 px-4 py-1 rounded-md bg-blue-900 w-24 font-medium">
-                        <Link href={'signin'} className="">Sign In</Link>
+                    <li className="text-neutral-100 bg-blue-800 sm:mt-0 px-4 py-2 rounded-md font-medium">
+                        <Link href={'signin'} className="">สมัครสมาชิก</Link>
                     </li>
                 </ul>
             </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,41 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
-      <main className="dark:bg-blue-900 h-max pt-20 px-8">
-        <h1 className="sm:text-7xl text-vw font-black leading-none">A company that you trust.</h1>
-        <div className="mt-20">
-          <button className="rounded-md bg-blue-700 mr-4 py-1 px-4 text-lg mb-4">Buy Policy Now</button>
-          <button className="rounded-md bg-blue-400 py-1 px-4 text-lg font-bold">Contact Us</button>
+      <main className="h-max pt-20 px-16">
+        <h1 className="mt-16 sm:mt-32 text-neutral-800 dark:text-white sm:text-7xl text-vw font-black leading-none">
+          A company <span className="text-blue-700 italic">that you trust.</span>
+        </h1>
+        <h3 className="mt-6 sm:mt-10 text-2xl font-extrabold text-neutral-800 dark:text-white">Looking for insurance? We got you covered.</h3>
+        <div className="mt-8 sm:mt-12 mb-20">
+          <button className="text-neutral-800 font-semibold rounded-md bg-white mr-4 py-2 px-4 mb-4 text-xl border-solid border-2 border-neutral-300 transition ease-in-out hover:-translate-y-1">Check Price</button>
+          <button className="text-white font-semibold rounded-md bg-blue-800 py-2 px-4 text-xl transition ease-in-out hover:-translate-y-1">Contact Us</button>
         </div>
       </main>
       <section>
-
       </section>
-      <footer>
+      <footer className="bg-blue-900 px-16 py-10 text-white sm:flex gap-6 justify-between">
+        <div className="mt-4">
+          <h4 className="font-semibold mb-1">เวลาทำการ</h4>
+          <p>จันทร์-เสาร์ (8:30-17:00)</p>
+        </div>
+        <div className="mt-4">
+          <h4 className="font-semibold mb-1">ที่อยู่-ติดต่อ</h4>
+          <p>บริษัท ไอเอสบี อินชัวรันส์โบรกเกอร์ จำกัด</p>
+          <p>95/2 หมู่5 ตำบลสำโรงเหนือ อำเภอเมือง สมุทรปราการ 10270</p>
+          <p>โทร: 027571717</p>
+        </div>
+        <div className="mt-4">
+          <h4 className="font-semibold mb-1">Social Media</h4>
+          <p className="underline underline-offset-2">
+            <Link href='https://page.line.me/ztc4820b'>Line Official</Link>
+          </p>
+          <p className="underline underline-offset-2">
+            <Link href='https://www.facebook.com/isbinsurancebroker/'>Facebook Page</Link>
+          </p>
+        </div>
+
 
       </footer>
     </>
